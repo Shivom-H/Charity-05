fdea25689273b7eb4472459f978e8970d7d1cc26
Shivom2003 <shivomh579@gmail.com>
2021-09-04 17:09:38 +0530

    updated

--- src/Components/card/component/cards.jsx
+++ src/Components/card/component/cards.jsx
@@ -4,7 +4,7 @@ import ProgressBar from "@ramonak/react-progress-bar";
 export default function Cards(props) {
   return (
     <div className="custom-cd-final">
-    <div className="card custom-card-2" style={{ width: "20rem" , margin: "0 0 20px 20px"}}>
+    <div className="card custom-card-2" style={{ width: "18rem" , margin: "0 0 20px 20px"}}>
       <img src={props.link} className="card-img-top" alt="..."></img>
       <div className="card-body">
         <h1 className="card-title text-light">{props.title} <i class="fas fa-clock"></i></h1>
